71bacd83454178745c1e5dc1df424bd8fe068ace
Ioan Tocitu <ioan.tocitu@totalsoft.ro>
2021-09-30 16:39:16 +0300

    end of 30.09

--- src/features/conference/conferenceDb.js
+++ src/features/conference/conferenceDb.js
@@ -1,24 +1,34 @@
-const {SQLDataSource} = require("../../utils/sqlDataSource")
-
-class ConferenceDb extends SQLDataSource{
-    generateWhereClause(queryBuilder, filters={}){
-        const{startDate,endDate} = filters
-        if (startDate) queryBuilder.andWhere("StartDate",">=", startDate)
-        if (endDate) queryBuilder.andWhere("StartDate","<=", endDate)
+const { SQLDataSource } = require("../../utils/sqlDataSource")
+const conferenceColumns = ["Id", "Name", "ConferenceTypeId", "LocationId", "CategoryId", "StartDate", "EndDate"]
+class ConferenceDb extends SQLDataSource {
+    generateWhereClause(queryBuilder, filters = {}) {
+        const { startDate, endDate, organizerEmail } = filters
+        if (startDate) queryBuilder.andWhere("StartDate", ">=", startDate)
+        if (endDate) queryBuilder.andWhere("StartDate", "<=", endDate)
+        if (organizerEmail) queryBuilder.andWhere('OrganizerEmail', organizerEmail)
     }
 
-    async getConferenceList(pager, filters){
-        const {page, pageSize} = pager
-        const values = await this.knex.select("Id","Name","ConferenceTypeId","LocationId","CategoryId","StartDate","EndDate")
-        .from('Conference')
-        .modify(this.generateWhereClause, filters)
-        .orderBy("Id")
-        .offset(page * pageSize)
-        .limit(pageSize)
-        return {values}
+    async getConferenceList(pager, filters) {
+        const { page, pageSize } = pager
+        const values = await this.knex
+            .select("Id", "Name", "ConferenceTypeId", "LocationId", "CategoryId", "StartDate", "EndDate")
+            .from('Conference')
+            .modify(this.generateWhereClause, filters)
+            .orderBy("Id")
+            .offset(page * pageSize)
+            .limit(pageSize)
+        return { values }
     }
-    async getConferenceListTotalCount(filters){
-        return await this.knex('Conference').count('Id',{ as: 'TotalCount'}).modify(this.generateWhereClause, filters).first()
+    async getConferenceListTotalCount(filters) {
+        return await this.knex('Conference').count('Id', { as: 'TotalCount' }).modify(this.generateWhereClause, filters).first()
+    }
+    async getConferenceById(id) {
+        const result = await this.knex
+            .select(...conferenceColumns)
+            .from('Conference')
+            .where('Id', id)
+            .first()
+            return result
     }
 }
 module.exports = ConferenceDb
